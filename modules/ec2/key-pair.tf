65da45c23999f1fc3db3d9da865d3a86bfda7d2e
Toan Duc Vo <toan.ducvo@icloud.com>
2024-01-06 07:14:20 +0000

    test

--- modules/ec2/key-pair.tf
+++ modules/ec2/key-pair.tf
@@ -1,25 +1,37 @@
-resource "null_resource" "delete_key_pair" {
+# Create a key pair and save the private key to local 
+resource "null_resource" "create_key_pair" {
   triggers = {
-    key_name = var.key_name
+    key_name    = var.key_name
     private_key = var.private_key
   }
 
   provisioner "local-exec" {
-    when    = destroy
-    on_failure = continue
-    command = "test -f ${self.triggers.private_key} && rm -f ${self.triggers.private_key} && aws ec2 delete-key-pair --key-name ${self.triggers.key_name} || echo 'No key to delete'"
+    when       = create
+    on_failure = fail
+    command    = "test -f ${self.triggers.private_key} || aws ec2 create-key-pair --key-name ${self.triggers.key_name} --query 'KeyMaterial' --output text > ${self.triggers.private_key} && chmod 400 ${self.triggers.private_key}"
   }
 }
 
-resource "null_resource" "create_key_pair" {
+
+# Save the private key in Secrets Manager
+resource "aws_secretsmanager_secret_version" "ec2-private-key-main" {
+  secret_id     = var.key_name
+  secret_string = file(var.private_key)
+
+  depends_on = [
+    null_resource.create_key_pair
+  ]
+}
+
+# remove the private key from local
+resource "null_resource" "delete_key_pair" {
   triggers = {
-    key_name = var.key_name
+    key_name    = var.key_name
     private_key = var.private_key
   }
 
   provisioner "local-exec" {
-    when = create
-    on_failure = fail
-    command = "test -f ${self.triggers.private_key} || aws ec2 create-key-pair --key-name ${self.triggers.key_name} --query 'KeyMaterial' --output text > ${self.triggers.private_key} && chmod 400 ${self.triggers.private_key}"
+    when       = destroy
+    command = "test -f ${self.triggers.private_key} && rm -f ${self.triggers.private_key} && aws ec2 delete-key-pair --key-name ${self.triggers.key_name} || echo 'No key to delete'"
   }
 }
